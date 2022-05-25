e9b9c934abffb31d80c1e86a7e087985235fc228
kopas2010 <nekomaneki135km@gmail.com>
2022-05-25 15:37:46 +0000

    0525-4

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
   has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true
 end
