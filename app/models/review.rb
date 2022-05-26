6e7ec5a9ab2cacbf8dd98466b36fb0d97c0c1b4e
kopas2010 <nekomaneki135km@gmail.com>
2022-05-26 08:10:37 +0000

    0526-1

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,10 @@ class Review < ApplicationRecord
   belongs_to :movie
   has_many :comments, dependent: :destroy
 
+  validates :star, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1}, presence: true
+
   validates :title, presence: true
   validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_034850) do
+ActiveRecord::Schema.define(version: 2022_05_26_061535) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2022_05_23_034850) do
     t.integer "movie_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "star", default: 0.0, null: false
   end
 
   create_table "user_relationships", force: :cascade do |t|
