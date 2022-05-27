646fda65196454e58ace1954772a42b667f580ae
kopas2010 <nekomaneki135km@gmail.com>
2022-05-27 13:58:03 +0000

    0527-1

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,6 +1,6 @@
 class User::HomesController < ApplicationController
   def top
-    @movies = Movie.order(created_at: :desc).limit(5)
-    @reviews = Review.order(created_at: :desc).limit(5)
+    @movies = Movie.order(created_at: :desc).limit(3)
+    @reviews = Review.order(created_at: :desc).limit(3)
   end
 end
