9b3127acc0fc42fef13cbde2e7b450c27a09e831
kopas2010 <nekomaneki135km@gmail.com>
2022-06-15 13:41:19 +0000

    0615-2

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -2,13 +2,13 @@ class User::FavoritesController < ApplicationController
 
   def create
     movie = Movie.find(params[:movie_id])
-    favorite = current_user.favorites.new(movie_id: movie.id)
-    favorite.save
+    @favorite = current_user.favorites.new(movie_id: movie.id)
+    @favorite.save
   end
 
   def destroy
     movie = Movie.find(params[:movie_id])
-    favorite = current_user.favorites.find_by(movie_id: movie.id)
-    favorite.destroy
+    @favorite = current_user.favorites.find_by(movie_id: movie.id)
+    @favorite.destroy
   end
 end
